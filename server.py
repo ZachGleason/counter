2d9f466ee40c64f1ad77b0bb7f3311b2ab0760f3
Zach <zgleason37@gmail.com>
2022-02-28 16:19:02 -0800

    first commit

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, session, redirect, request
+app = Flask(__name__)
+app.secret_key = "countit"
+
+@app.route('/')
+def index():
+    if "count" not in session:
+        session["count"] = 1
+    else:
+        session["count"] += 1
+
+    return render_template("index.html")
+
+@app.route('/count', methods=["POST"])
+def count():
+    if request.form["alter"]== "add":
+        session["count"] += 1
+    elif request.form["alter"]== "reset":
+        session["count"] = 0
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
